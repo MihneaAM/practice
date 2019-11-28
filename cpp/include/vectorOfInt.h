2b3942fb6fa68756b6dde83008c235fa423cdeb7
Mihnea Moldovan <mihnea.moldovan@gmail.com>
2019-11-28 14:49:39 +0200

    implement vector class

---
+++ cpp/include/vectorOfInt.h
@@ -0,0 +1,26 @@
+#ifndef VECTOR_OF_INT_H
+#define VECTOR_OF_INT_H
+
+class vectorOfInt
+{
+    public:
+
+    vectorOfInt ();
+    vectorOfInt (int);
+    int get (int);
+    void set (int, int);
+    void push_back (int);
+    void push_front (int);
+    vectorOfInt& operator= (const vectorOfInt &);
+    vectorOfInt (const vectorOfInt &);
+    int size ();
+    void print ();
+
+    private:
+    
+    int *arr;
+    int curr_index;
+    int capacity;
+};
+
+#endif
